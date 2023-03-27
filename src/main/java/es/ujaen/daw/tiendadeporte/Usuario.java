6005414683c477ac93fd47049b49f29f6e0af6ab
amp00123 <99319967+amp00123@users.noreply.github.com>
2023-03-27 19:02:09 +0200

    ModificaUsuario

--- src/main/java/es/ujaen/daw/tiendadeporte/Usuario.java
+++ src/main/java/es/ujaen/daw/tiendadeporte/Usuario.java
@@ -1,43 +1,68 @@
 package es.ujaen.daw.tiendadeporte;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.Id;
 
 import java.io.Serializable;
+import java.time.LocalDate;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.Size;
 
-@Entity()
-public class Usuario implements Serializable{
-    @Id
+@Entity()       //JPA annotations
+//@Table(name="Usuario")
+public class Usuario implements Serializable {
+
+    @Id         //JPA annotations
+    @GeneratedValue(strategy = GenerationType.IDENTITY) //Auto-incremental
     private Integer id;
-    private String dni;
-    private String nombre;
 
-    //todo cambiar el orden de los atributos, primero nombre y despues dni
-    //todo poner el id autoincremental
+    @Size(min = 4, max = 25, message = "La longitud ${validatedValue} debe estar entre {min} y {max} caracteres")
+    private String nombre;
+    @Pattern(regexp = "\\d{7,8}(-?[a-zA-Z])?", message = "{Usuario.dni.formato}")
+    private String dni;
+
+
 
 
     public Usuario() {
         id = 0;
-        dni = "";
-        nombre = "";
+        nombre = "Desconocido";
+
     }
 
-    public Usuario(Integer id, String dni, String nombre) {
+    public Usuario(Integer id, String nombre, String dni) {
         this.id = id;
-        this.dni = dni;
         this.nombre = nombre;
+        this.dni = dni;
+
     }
 
     /**
-     * Constructor de copia
+     * Copy constructor
      */
     public Usuario(Usuario u) {
         this.id = u.id;
-        this.dni = u.dni;
         this.nombre = u.nombre;
+        this.dni = u.dni;
+
     }
 
+    /**
+     * @return the id
+     */
+    public Integer getId() {
+        return id;
+    }
 
+    /**
+     * @param id the id to set
+     */
+    public void setId(Integer id) {
+        this.id = id;
+    }
 
     /**
      * @return the nombre
@@ -47,12 +72,18 @@ public class Usuario implements Serializable{
     }
 
     /**
+     * @param nombre the nombre to set
+     */
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    /**
      * @return the dni
      */
     public String getDni() {
         return dni;
     }
-
 
     /**
      * @param dni the dni to set
@@ -61,22 +92,122 @@ public class Usuario implements Serializable{
         this.dni = dni;
     }
 
-    /**
-     * @param nombre the nombre to set
-     */
-    public void setNombre(String nombre) {
-        this.nombre = nombre;
-    }
 
 
-    public void setId(Integer id) {
-        this.id = id;
+
+    @Override
+    public boolean equals(Object object) {
+        if (!(object instanceof Usuario)) {
+            return false;
+        }
+        Usuario other = (Usuario) object;
+        return this.id == other.id;
     }
 
-    public Integer getId() {
-        return id;
+    @Override
+    public int hashCode() {
+        int hash = 7;
+        hash = 11 * hash + this.id;
+        return hash;
     }
+
+    @Override
+    public String toString() {
+        return "Usuario[ id=" + id +"]";
 }
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
