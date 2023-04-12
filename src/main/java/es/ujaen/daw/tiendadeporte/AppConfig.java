dd1aca7df93dac7bd28fcd00f44018984fb86748
lmv00019 <124671238+lmv00019@users.noreply.github.com>
2023-04-12 17:43:21 +0200

    Actualizacion AppConfig

--- src/main/java/es/ujaen/daw/tiendadeporte/AppConfig.java
+++ src/main/java/es/ujaen/daw/tiendadeporte/AppConfig.java
@@ -1,7 +1,17 @@
 package es.ujaen.daw.tiendadeporte;
 
 import jakarta.faces.annotation.FacesConfig;
+import jakarta.enterprise.context.ApplicationScoped;
+import org.glassfish.soteria.identitystores.annotation.Credentials;
+import org.glassfish.soteria.identitystores.annotation.EmbeddedIdentityStoreDefinition;
 
+
+
+@EmbeddedIdentityStoreDefinition({
+        @Credentials(callerName = "admin", password = "secret1", groups = {"ADMINISTRADORES"}),
+        @Credentials(callerName = "user", password = "secret2", groups = {"USUARIOS"})
+})
+@ApplicationScoped
 @FacesConfig
 public class AppConfig {
 }
